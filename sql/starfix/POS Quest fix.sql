ce0f1ec2a0b494cae7497a1c22f68c5eadc1bec3
st4rgaze <st4rgaze@gmail.com>
2011-02-21 21:13:46 +0700

    fix typo, terusan q pos, set guid leerooy yg ktinggalan

--- sql/starfix/POS Quest fix.sql
+++ sql/starfix/POS Quest fix.sql
@@ -1,1 +1,9 @@
+-- ke Pos
 UPDATE `creature_template` SET `faction_A` = 534,`faction_H` = 534 WHERE `entry` in (37597); 
+
+-- bebasin slave horde
+UPDATE quest_template SET ReqCreatureOrGOId1 = -201969 WHERE entry = 24507;
+
+-- slave ally
+UPDATE quest_template SET ReqCreatureOrGOId1 = -201969 WHERE entry = 24498;
+
